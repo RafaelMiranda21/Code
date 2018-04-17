3754e26a03aba6be19f7712d4e0d9ea30f1bc817
RafaelMiranda21 <38355955+RafaelMiranda21@users.noreply.github.com>
2018-04-16 22:32:16 -0300

    Update metodos.cpp

--- metodos.cpp
+++ metodos.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-int Interface::getnumero()
-{
-    return numero;
-}
 
 void Interface::setnumero(int x)
 {
@@ -18,31 +14,16 @@ void Interface::setnumero(int x)
     numero = x;
 }
 
-bool Interface::numeroperfeito(int x)
+
+int Interface::getnumero()
 {
-    int i,soma=0;
-
-	for(i=1 ; i<=(numero/2);i++)
-	{
-		if(numero%i == 0)
-		{
-			soma = soma + i;
-		}
-
-
-	}
-	if(soma == numero)
-	{
-		return true;
-	}
-	    return false;
-
+    return numero;
 }
 
-int Interface::fatorial(int x)
+ int Interface::fatorial()
 {
     int resultado = 1;
-   for(int i = 1; i <= x ;i++)
+    for(int i = 1; i <= numero ;i++)
     {
 	  resultado = resultado * i;
     }
@@ -52,3 +33,187 @@ int Interface::fatorial(int x)
 
 
 
+
+bool Interface::numeroperfeito()
+{
+    int i,soma=0;
+
+	for(i=1 ; i<=(numero/2);i++)
+	{
+		if(numero%i == 0)
+		{
+			soma = soma + i;
+		}
+	}
+	if(soma == numero)
+	{
+		return true;
+	}
+	    return false;
+
+}
+
+
+bool Interface::capicua()
+{
+    int i,casas = 1,numero1,numero2,j;
+	numero1 = numero;
+	numero2 = numero;
+
+	while((numero1 = numero1/10) > 0)
+    {
+    	casas++;
+	}
+	int aux[casas];
+	cout<<"\n\n";
+
+	for(i=0;i<casas;i++)
+	{
+		aux[i] = numero2%10;
+		numero2 = numero2/10;
+
+
+	}
+	cout<<"\n\n";
+	for(i=0,j=(casas-1);i<casas && aux[i] == aux[j]; i++,j--)
+	{
+
+
+	}
+	if(i == (casas))
+	  {
+	  	return true;
+	  }
+	   return false;
+
+}
+
+bool Interface::quadradoperfeito()
+{
+   int termos = 1, soma = 0;
+
+   for(termos,soma;soma<numero;termos++)
+   {
+ 	soma= termos*(2+(termos-1)*2)/2;
+   }
+
+   if(soma==numero)
+   {
+ 	return true;
+   }
+    return false;
+
+}
+
+
+bool Interface::numeroprimo()
+{
+ int i,cont=1;
+
+	for(i=1;i<=numero/2;i++)
+	{
+		if(numero%i == 0)
+		{
+			cont++;
+		}
+	}
+
+	if(cont == 2)
+	{
+		return true;
+	}
+	return false;
+}
+
+
+int Interface::mdc(Interface y)
+{
+    int maior = 0,i;
+
+    if(numero > y.numero)
+    {
+    	for(i=1;i<=(y.numero/2);i++)
+       {
+        if(numero%i == 0 && y.numero%i == 0)
+        {
+            maior = i;
+        }
+       }
+
+	}
+
+    else
+   {
+
+    for(i=1;i<=(numero/2);i++)
+    {
+        if(numero%i == 0 && y.numero%i == 0)
+        {
+            maior = i;
+        }
+    }
+
+   }
+    return maior;
+
+}
+
+int Interface::mmc(Interface y)
+{
+    if(numero >= y.numero)
+	{
+		if(numero%y.numero == 0)
+		{
+			return numero;
+		}
+		else
+		{
+			return numero * y.numero;
+		}
+	}
+	else
+	{
+		if(y.numero%numero == 0)
+		{
+			return y.numero;
+		}
+		else
+		{
+			return numero * y.numero;
+		}
+	}
+}
+
+
+bool Interface::numeroprimoentresi(Interface y)
+{
+    int i,igual=0;
+
+	if(numero < y.numero)
+	{
+
+	   for(i=1;i<=numero/2;i++)
+	   {
+		   if(numero%i == 0 && y.numero%i == 0)
+		   {
+			  igual++;
+		   }
+	   }
+   }
+   else
+   {
+	for(i=1;i<=y.numero/2;i++)
+	{
+		if(y.numero%i == 0 && numero%i == 0)
+		{
+			igual++;
+		}
+	}
+  }
+
+  if(igual == 1)
+  {
+  	return true;
+  }
+    return false;
+}
